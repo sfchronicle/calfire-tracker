68e679281578614f9146f34fb475049cfcd9e234
Erica Yee <eeyusongen@gmail.com>
2019-06-10 16:43:09 -0700

    parse new json url

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,27 @@
-(function(){
-var _$main_1 = {};
-'use strict';
-
-// https://sfc-project-files.s3.amazonaws.com/project-feeds/fire_tracker_c2p_firedata_2019.json
-// C2P UPDATES BELOW URL ON DEPLOY, BUT UNABLE TO PARSE
-// https://files.sfchronicle.com/project-feeds/FireData_2019.sheet.json
 $.get("https://files.sfchronicle.com/project-feeds/FireData_2019.sheet.json", function (data) {
 
-	// DON'T HAVE LAST UPDATED FIELD IN JSON FILE, SO JUST RETURNING CURRENT TIME FOR NOW
+	// TODO: PARSE 'UPDATED' FIELD IN JSON FILE
+	// when done, also uncomment .updated div
+
 	// update date
 	// var updated = data[0].Update;
-	var updated = new Date();
-	$('#updated').append(updated);
+	// $('#updated').append(updated);
 
+	// TODO: PRIORITIZE MAJOR FIRES
 	// get the first 4 fires
-	var fires = data.splice(0, 4);
+	var fires = jQuery.parseJSON(data).splice(0,4);
 
 	// get fire data from each
 	fires.forEach(function (fire) {
 
 		var title = fire.FireName,
-		    slug = fire.FireName.replace(/[|&;$%@"<>()+,\s]+/g, '-').toLowerCase(),
-		    acreage = fire.Acreage,
-		    agency = fire.Agency,
-		    description = fire.Description,
-		    source = fire.Source,
-		    startDate = fire.StartDate,
-		    containment = fire.Containment;
+		slug = fire.FireName.replace(/[|&;$%@"<>()+,\s]+/g, '-').toLowerCase(),
+		acreage = fire.Acreage,
+		agency = fire.Agency,
+		description = fire.Description,
+		source = fire.Source,
+		startDate = fire.StartDate,
+		containment = fire.Containment;
 
 		// save each fire data html
 		var fireDiv = "<div class='fire " + slug + "'>" + "<div class='title'>" + title + "</div>" +
@@ -35,7 +30,6 @@ $.get("https://files.sfchronicle.com/project-feeds/FireData_2019.sheet.json", fu
 
 		// print each fire data html
 		$('#fires').append(fireDiv);
+
 	});
 });
-
-}());
